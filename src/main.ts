b9e0e89a22880e22a2209d288d5a8768ce7f7720
Manabu Sakai <rea.527@gmail.com>
2020-06-30 17:00:44 +0900

    Add column for recovery time

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,8 @@ async function run(): Promise<void> {
     runID: process.env.GITHUB_RUN_ID
   })
 
+  const startTime = new Date() // UTC
+
   try {
     // REGISTRY_NAME はユーザー側から渡せない様にする
     const registry: string | undefined = process.env.REGISTRY_NAME
@@ -24,8 +26,6 @@ async function run(): Promise<void> {
     if (process.env.GITHUB_TOKEN) {
       core.setSecret(process.env.GITHUB_TOKEN)
     }
-
-    const startTime = new Date() // UTC
 
     const target = core.getInput('target')
     core.debug(`target: ${target}`)
@@ -70,7 +70,7 @@ async function run(): Promise<void> {
     }
 
     const endTime = new Date() // UTC
-    s3.uploadBuildTime(startTime, endTime)
+    s3.uploadBuildTime(startTime, endTime, imageName, 'success', 'NoError')
 
     const elapsedSec = (endTime.getTime() - startTime.getTime()) / 1000
     const buildTime = `${Math.floor(elapsedSec / 60)}min ${elapsedSec % 60}sec`
@@ -81,17 +81,27 @@ async function run(): Promise<void> {
       docker.builtImage?.tags.join(', ')
     )
   } catch (e) {
+    let buildReason: string
     if (e instanceof BuildError) {
+      buildReason = 'BuildError'
       core.error('image build error')
       notification.notifyBuildFailed(thisAction)
     } else if (e instanceof ScanError) {
+      buildReason = 'ScanError'
       core.error('image scan error')
     } else if (e instanceof PushError) {
+      buildReason = 'PushError'
       core.error('ecr push error')
     } else {
+      buildReason = 'UnknownError'
       core.error(e.message)
       core.error('unknown error')
     }
+
+    const endTime = new Date() // UTC
+    const imageName = core.getInput('image_name')
+    s3.uploadBuildTime(startTime, endTime, imageName, 'fail', buildReason)
+
     core.setFailed(e)
   }
 }
